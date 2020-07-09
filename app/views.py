1772428c4d1a740542fe6f59b9ba7f69817af549
mishael254 <mishaelmishantoz@gmail.com>
2020-07-09 12:05:34 +0300

    create a new movie page function that returns the movie details page and its data

--- app/views.py
+++ app/views.py
@@ -6,4 +6,11 @@ def index():
     '''
     a view route page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message = 'hello dickface'
+    return render_template('index.html',message = message)
+@app.route('/movie/<movie_id>')
+def movie (movie_id):
+    '''
+    new movie page function that returns the movie details page and its data
+    '''
+    return render_template('movie.html',id = movie_id)
