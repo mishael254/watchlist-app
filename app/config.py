be4d3159473ef26b02fcc55f8ea05c76ee318ffb
mishael254 <mishaelmishantoz@gmail.com>
2020-07-09 12:37:23 +0300

    create various classes in our config.py so as to come up with various configurations for the app to meet the required needs

---
+++ app/config.py
@@ -0,0 +1,17 @@
+class Config:
+    """
+    general configuration parent class
+    """
+    pass
+class ProdConfig(Config):
+    '''
+    production configuration child class
+    arguments config this is the parent class
+    '''
+    pass
+class DevConfig(Config):
+    '''
+    development configuration child class
+    '''
+    pass
+Debug = True
