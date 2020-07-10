893973f148b8fa2ae9972088bd3fff350af396ec
mishael254 <mishaelmishantoz@gmail.com>
2020-07-10 15:45:17 +0300

    placed the API url inside the main class class config and assigned it to a variable movie api base url

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ class Config:
     """
     general configuration parent class
     """
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=7fc17f781f7ad8fa7e2c2c1b211fcfe0'
     pass
 class ProdConfig(Config):
     '''
