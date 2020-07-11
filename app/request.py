1c7db5b8aa206bc44eb1a9eac2430d0877e0dff2
mishael254 <mishaelmishantoz@gmail.com>
2020-07-11 20:04:13 +0300

    import app instance ,also imported the urllib request module also imported movie module from the models folder also caught the API key and assigned it in to a variable named api_key caught also the MOVIE_BASE_URL and also assigned it into a variable named base url

--- app/request.py
+++ app/request.py
@@ -1,3 +1,12 @@
 from app import app
+import urllib.request,json
+from .models import movie
+
+Movie = movie.Movie
+
 #getting the API key
 api_key = app.config['MOVIE_API_KEY']
+
+# getting the movie base url
+base_url = app.config['MOVIE_API_BASE_URL']
+
