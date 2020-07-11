957f7567c30cef3ec0623467039cda44f684a60f
mishael254 <mishaelmishantoz@gmail.com>
2020-07-11 18:02:29 +0300

    create a test case to ensure that objects are instanciated correctly

--- app/movie_test.py
+++ app/movie_test.py
@@ -12,6 +12,18 @@ class MovieTest(unittest.TestCase):
         '''
         self.new_movie = Movie(1234,'python must be crazy','a thrilling new python series','https://image.tmdb.org/t/p/w500/khsjha27hbs',8.5,129993)
 
+    def test_init(self):
+        '''
+        a test case to ensure that objects are instanciated correctly
+        '''
+        self.assertEqual(self.new_movie.id,1234)
+        self.assertEqual(self.new_movie.title,'python must be crazy')
+        self.assertEqual(self.new_movie.overview,'a thrilling new python series')
+        self.assertEqual(self.new_movie.poster,'https://image.tmdb.org/t/p/w500/khsjha27hbs')
+        self.assertEqual(self.new_movie.vote_average,8.5)
+        self.assertEqual(self.new_movie.vote_count,12993)
+
+
     def test_instance(self):
             self.assertTrue(isinstance(self.new_movie,Movie))
 
