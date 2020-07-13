b7074127387b9a60c090a34816a756b8a1c82997
mishael254 <mishaelmishantoz@gmail.com>
2020-07-13 18:02:00 +0300

    new variables and used the get function to pass in them

--- app/views.py
+++ app/views.py
@@ -9,10 +9,13 @@ def index():
     '''
     # getting popular movies
     popular_movies = get_movies('popular')
-    print(popular_movies)
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
+
+
 
     title = 'welcome to the best web movie review online'
-    return render_template('index.html',title = title,popular = popular_movies)
+    return render_template('index.html',title = title,popular = popular_movies,upcoming = upcoming_movie,now_showing = now_showing_movie)
 @app.route('/movie/<int:movie_id>')
 def movie (movie_id):
     '''
