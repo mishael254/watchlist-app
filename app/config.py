55432c5c3d95a29e6239f2027779a523c054cb5c
mishael254 <mishaelmishantoz@gmail.com>
2020-07-14 10:35:18 +0300

    included the api key in the class config

--- app/config.py
+++ app/config.py
@@ -2,6 +2,7 @@ class Config:
     """
     general configuration parent class
     """
+    MOVIE_API_KEY = '8a21c603e77fea3673a707f5e3736e64'
     MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=8a21c603e77fea3673a707f5e3736e64'
     pass
 class ProdConfig(Config):
