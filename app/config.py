4065f71c5fafecf960ee655f0116e1d274460cda
mishael254 <mishaelmishantoz@gmail.com>
2020-07-14 17:02:44 +0300

    remove the pass keyword

--- app/config.py
+++ app/config.py
@@ -3,8 +3,8 @@ class Config:
     general configuration parent class
     """
     MOVIE_API_KEY = '8a21c603e77fea3673a707f5e3736e64'
-    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/550?api_key=8a21c603e77fea3673a707f5e3736e64'
-    pass
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+    
 class ProdConfig(Config):
     '''
     production configuration child class
