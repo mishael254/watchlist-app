717ce493e959bd56a273d9d00d4b9c118d5d45f5
mishael254 <mishaelmishantoz@gmail.com>
2020-07-14 17:03:34 +0300

    assign both the API and the url to null

--- app/request.py
+++ app/request.py
@@ -2,11 +2,11 @@ from app import app
 import urllib.request,json
 from .models import movie
 
-Movie = movie.Movie
+
 
 #getting the API key
-api_key = app.config['MOVIE_API_KEY']
-base_url = app.config['MOVIE_API_BASE_URL']
+api_key = None
+base_url = None
 
 # getting the movie base url
 
