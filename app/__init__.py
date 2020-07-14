2414b5d7a4f9e9ba255e255aee60ced470070d6b
mishael254 <mishaelmishantoz@gmail.com>
2020-07-15 02:26:59 +0300

    import error from app module there down next to imported  views

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from.config import DevConfig
 from flask_bootstrap import Bootstrap
+
+
 # initializing the application
 app = Flask(__name__,instance_relative_config=True)
 
@@ -12,3 +14,4 @@ app.config.from_pyfile('config.py')
 bootstrap = Bootstrap(app)
 
 from app import views
+from app import error
