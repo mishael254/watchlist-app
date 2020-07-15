357f3ead45f703b3354d082574fae4d1fb9d2a4c
mishael254 <mishaelmishantoz@gmail.com>
2020-07-15 10:51:22 +0300

    import the render_template that  comes with request and redirect and url-for  tools

--- app/views.py
+++ app/views.py
@@ -2,6 +2,8 @@ from flask import render_template
 from app import app
 from .request import get_movies
 from .request import get_movies,get_movie
+from .request import get_movies,get_movie,search_movie
+from flask import render_template,request,redirect,url_for
 
 @app.route('/')
 def index():
@@ -16,7 +18,13 @@ def index():
 
 
     title = 'welcome to the best web movie review online'
-    return render_template('index.html',title = title,popular = popular_movies,upcoming = upcoming_movie,now_showing = now_showing_movie)
+
+    search_movie = request.args.get('movie_query')
+
+    if search_movie:
+        return redirect(url_for('search',movie_name = search_movie))
+    else:
+        return render_template('index.html',title = title,popular = popular_movies,upcoming = upcoming_movie,now_showing = now_showing_movie)
 @app.route('/movie/<int:id>')
 def movie (id):
     '''
@@ -26,3 +34,14 @@ def movie (id):
     movie = get_movie(id)
     title = f'{movie.title}'
     return render_template('movie.html', title = title , movie = movie )
+# searching movies
+@app.route('/search/<movie_name>')
+def search(movie_name):
+    '''
+    view function to display search results
+    '''
+    movie_name_list = movie_name.split(" ")
+    movie_name_format = "+".join(movie_name_list)
+    searched_movies = search_movie(movie_name_format)
+    title = f'search results for {movie_name}'
+    return render_template('search.html', movies = searched_movies, title = title)
